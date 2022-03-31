b0318c102d5cc617ecb14eeab94dd519af666097
Ray Brown-Amory <r.brown-amory@sky.com>
2022-03-31 14:34:58 +0100

    created the basket enities

---
+++ src/Services/Basket/Basket.API/Entites/ShoppingCartCartItem.cs
@@ -0,0 +1,11 @@
+﻿namespace Basket.API.Entites
+{
+    public class ShoppingCartCartItem
+    {
+        public int Quantity { get; set; }
+        public string Color { get; set; }
+        public decimal Price { get; set; }
+        public string ProductId { get; set; }
+        public string ProductName { get; set; }
+    }
+}
