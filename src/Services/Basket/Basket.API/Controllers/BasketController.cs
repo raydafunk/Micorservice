d95fd08660730f089bbe185270a0da9db2d2e4db
Ray Brown-Amory <r.brown-amory@sky.com>
2022-04-03 22:38:45 +0100

    skeleton of the basket controller

---
+++ src/Services/Basket/Basket.API/Controllers/BasketController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Basket.API.Controllers
+{
+    [ApiController]
+    [Route("api/v1/[controller]")]
+    public class BasketController : ControllerBase
+    {
+
+    }
+}
