6db192e9f77dcbf669e48203e860adb9fa935748
Ray Brown-Amory <r.brown-amory@sky.com>
2022-04-03 22:23:33 +0100

    added the Interface  and  Repo for the Basket ApI

---
+++ src/Services/Basket/Basket.API/Repositories/Interfaces/IBasketRepository.cs
@@ -0,0 +1,12 @@
+﻿using Basket.API.Entites;
+using System.Threading.Tasks;
+
+namespace Basket.API.Repositories.Interfaces
+{
+    public interface IBasketRepository
+    {
+        Task<ShoppingCart> GetBasket(string Username);
+        Task<ShoppingCart> UpdateBasket(ShoppingCart basket);
+        Task DeleteBasket(string Username);
+    }
+}
--- src/Services/Basket/Basket.API/Startup.cs
+++ src/Services/Basket/Basket.API/Startup.cs
@@ -1,15 +1,9 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
 using Microsoft.OpenApi.Models;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace Basket.API
 {
@@ -25,6 +19,10 @@ namespace Basket.API
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddStackExchangeRedisCache(opitons =>
+            {
+                opitons.Configuration = Configuration.GetValue<string>("CatcheSettings:ConnetionStrings");
+            });
 
             services.AddControllers();
             services.AddSwaggerGen(c =>
