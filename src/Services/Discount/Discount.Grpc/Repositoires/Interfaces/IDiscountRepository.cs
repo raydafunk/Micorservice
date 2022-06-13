58968fba9e4c06e74beb623faeb9ae32dc1c766a
Ray Brown-Amory <r.brown-amory@sky.com>
2022-06-13 14:38:42 +0100

    Seting up the grcp repo and files

---
+++ src/Services/Discount/Discount.Grpc/Repositoires/Interfaces/IDiscountRepository.cs
@@ -0,0 +1,14 @@
+﻿using Discount.Grpc.Entites;
+using System.Threading.Tasks;
+
+namespace Discount.Grpc.Repositoires.Interfaces
+{
+    public interface IDiscountRepository
+    {
+        Task<Coupon> GetDiscount(string productname);
+
+        Task<bool> CreateDiscount(Coupon coupon);
+        Task<bool> UpdateDiscount(Coupon coupon);
+        Task<bool>DeleteDiscount(string productname);
+    }
+}
--- src/Services/Discount/Discount.Grpc/Startup.cs
+++ src/Services/Discount/Discount.Grpc/Startup.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.AspNetCore.Builder;
+﻿using Discount.Grpc.Repositoires;
+using Discount.Grpc.Repositoires.Interfaces;
+using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
@@ -16,6 +18,7 @@ namespace Discount.Grpc
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddScoped<IDiscountRepository, DiscountRepository>();
             services.AddGrpc();
         }
 
