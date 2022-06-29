1be480b03c44244280e9df5e59a8ee0e06bce09b
Ray Brown-Amory <r.brown-amory@sky.com>
2022-06-29 13:34:11 +0100

    Finished the contracts in the Application layers for the ordering api

---
+++ src/Services/Ordering/Ordering.Application/Contracts/Persistence/IOrderRepository.cs
@@ -0,0 +1,11 @@
+﻿using Ordering.Domain.Enities;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Ordering.Application.Contracts.Persistence
+{
+    public interface IOrderRepository : IAsyncRepository<Order>
+    {
+        Task<IAsyncEnumerable<Order>> GetOrdersByUserName(string userName);
+    }
+}
