a2bde23dbe2de52929834269c17f4af30314d25f
Ray Brown-Amory <r.brown-amory@sky.com>
2022-07-06 15:26:55 +0100

    Implememted the order for the entity framework

---
+++ src/Services/Ordering/Ordering.Infrastructure/Presistance/OrderContext.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Ordering.Domain.Common;
+using Ordering.Domain.Enities;
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Ordering.Infrastructure.Presistance
+{
+    public class OrderContext : DbContext
+    {
+        public OrderContext(DbContextOptions<OrderContext> option) : base(option)
+        {
+                
+        }
+        public DbSet<Order> Orders { get; set; }
+
+        public override Task<int> SaveChangesAsync(CancellationToken cancellationToken = default)
+        {
+            foreach (var entry in ChangeTracker.Entries<EntityBase>())
+            {
+                switch (entry.State)
+                {
+                    case EntityState.Added:
+                        entry.Entity.CreatedDate = DateTime.Now;
+                        entry.Entity.CreatedBy = "rbc";
+                        break;
+                    case EntityState.Modified:
+                        entry.Entity.LastModifiedDate = DateTime.Now;
+                        entry.Entity.LastModifiedBy = "rbc";
+                        break;
+                }
+            }
+            return base.SaveChangesAsync(cancellationToken);
+        }
+    }
+}
---
+++ src/Services/Ordering/Ordering.Infrastructure/Presistance/OrderContextSeedcs.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.Extensions.Logging;
+using Ordering.Domain.Enities;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Ordering.Infrastructure.Presistance
+{
+    public  class OrderContextSeedcs
+    {
+        public static async Task CreateSeedcs(OrderContext orderContext, ILogger<OrderContextSeedcs> logger)
+        {
+            if (!orderContext.Orders.Any())
+            {
+                orderContext.Orders.AddRange(GetPreconfiguredOrders());
+                await orderContext.SaveChangesAsync();
+                logger.LogInformation("Seed database assicoated  wit the context {DbContextName}", typeof(OrderContext).Name);
+            }
+
+           
+        }
+
+       private  static IEnumerable<Order> GetPreconfiguredOrders()
+        {
+            return new List<Order>
+            {
+                new Order() {UserName = "rbc", FirstName = "Ray", LastName ="Brown-Amory", EmailAddress = "raybrown@exmaple", AddressLine ="Banchelor street",Country ="England"}
+            };
+        }
+
+    }
+}
