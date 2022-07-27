9a7e8fefd4001f1bd9c8fd92d247b540e788f07c
Ray Brown-Amory <r.brown-amory@sky.com>
2022-07-28 00:33:17 +0100

    Added class nfrastructureServiceReqistration

---
+++ src/Services/Ordering/Ordering.Infrastructure/InfrastructureServiceReqistration.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Ordering.Application.Contracts.Infrastructure;
+using Ordering.Application.Contracts.Persistence;
+using Ordering.Application.Models;
+using Ordering.Infrastructure.Mail;
+using Ordering.Infrastructure.Presistance;
+using Ordering.Infrastructure.Repositories;
+
+namespace Ordering.Infrastructure
+{
+    public static class InfrastructureServiceReqistration
+    {
+        public static IServiceCollection AddInfrastructureServices( this IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddDbContext<OrderContext>(opitons =>
+             opitons.UseSqlServer(configuration.GetConnectionString("OrderingConnectionString")));
+
+            services.AddScoped(typeof(IAsyncRepository<>), typeof(RepositoryBase<>));
+            services.AddScoped<IOrderRepository, OrderRespository>();
+
+            services.Configure<EmailSettings>(c => configuration.GetSection("EmailSettings"));
+            services.AddTransient<IEmailService, EmailService>();
+
+            return services;
+        }
+    }
+}
